06d93686dc138d8a6eba91fe866d170c9dfd2ffa
John Nikolakis <grjohnis@yahoo.gr>
2013-07-07 17:01:24 +0200

    Changed plugin boilerplate. Added public methods for next/previous. Added onBeforeNext/onBeforePrevious hooks

--- js/global.js
+++ js/global.js
@@ -1,15 +1,46 @@
+if (!window.console) {
+	console = {
+		log: function(){}
+	};
+}
+
 $(document).ready(function(){
 
 	$('#android_carousel').liquidCarousel({
 		height: 108,
-		hideNavigation: false
+		hideNavigation: false,
+		onBeforeNext: function() {
+			console.log('%conBeforeNext', 'color: green; font-weight:bold;', ' has been called');
+		},
+		onBeforePrevious: function() {
+			console.log('%conBeforePrevious', 'color: green; font-weight:bold;', ' has been called');
+		}
 	});
+
+	$('#androidPrevious').click(function(){
+		$('#android_carousel').liquidCarousel('previous');
+	});
+
+	$('#androidNext').click(function(){
+		$('#android_carousel').liquidCarousel('next');
+	});
+
+
+
 
 	$('#partheni_carousel').liquidCarousel({
 		height: 170,
 		hideNavigation: true,
 		animationDuration: 800,
 		noTransitions: true
+	});
+
+	$('#partheniPrevious').click(function(){
+		$('#partheni_carousel').liquidCarousel('previous');
+	});
+
+	$('#partheniNext').click(function(){
+		$('#partheni_carousel').liquidCarousel('next');
 	});
 
 });
